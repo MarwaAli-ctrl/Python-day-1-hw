4adeb6d5c4bcd6989d3363846b17cb5ccf6b530b
MarwaAli-ctrl <85699187+MarwaAli-ctrl@users.noreply.github.com>
2021-06-22 07:24:16 -0700

    Add files via upload

---
+++ python day 1 hw.py
@@ -0,0 +1,559 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Week 2 - Monday Lesson (variable assignment, loops, lists)
+
+# ## Tasks Today:
+# 
+# 1) Int & Float assignments <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; a) Assigning int <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; b) Assigning float <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; c) Performing Calculations on ints and floats <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Addition <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Subtraction <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Multiplication <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Division <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Floor Division <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Modulo <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; - Exponential <br>
+# 2) String Input-Output <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; a) String Assignment <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; b) print() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; c) String Concatenation <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; d) Type Conversion <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; e) input() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; f) format() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; g) Old Way (python 2) <br>
+# 3) <b>In-Class Exercise #1</b> <br>
+# 4) If Statements <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; a) 'is' keyword <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; b) 'in' keyword <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; c) 'not in' keyword <br>
+# 5) <b>In-Class Exercise #2</b> <br>
+# 6) Elif Statements <br>
+# 7) Else Statements <br>
+# 8) <b>In-Class Exercise #3</b> <br>
+# 9) For Loops <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; a) Using 'in' keyword <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; b) Continue Statement <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; c) Break Statement <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; d) Pass Statement <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; e) Double For Loops <br>
+# 10) While Loops <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; a) Looping 'While True' <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; b) While and For Loops Used Together <br>
+# 11) Built-In Functions <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; a) range() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; b) len() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; c) help() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; d) isinstance() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; e) abs() <br>
+# 12) Try and Except <br>
+# 13) Lists <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; a) Declaring Lists <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; b) Indexing a List <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; c) .append() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; d) .insert() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; e) .pop() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; f) .remove() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; g) del() <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; h) Concatenating Two Lists <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; i) Lists Within Lists <br>
+#  &nbsp;&nbsp;&nbsp;&nbsp; j) Looping Through Lists <br>
+
+# ### Int & Float Assignments
+
+# ##### Assigning int
+
+# In[ ]:
+
+
+
+
+
+# ##### Assinging float
+
+# In[ ]:
+
+
+
+
+
+# #### Performing Calculations on ints and floats
+
+# ##### Addition
+
+# In[ ]:
+
+
+
+
+
+# ##### Subtraction
+
+# In[ ]:
+
+
+
+
+
+# ##### Multiplication
+
+# In[ ]:
+
+
+
+
+
+# ##### Division
+
+# In[ ]:
+
+
+
+
+
+# ##### Floor Division
+
+# In[ ]:
+
+
+
+
+
+# ##### Modulo
+
+# In[ ]:
+
+
+
+
+
+# ##### Exponential
+
+# In[ ]:
+
+
+
+
+
+# ### String Input-Output
+
+# ##### String Assignment
+
+# In[ ]:
+
+
+
+
+
+# ##### print() <br>
+# <p>Don't forget about end=' '</p>
+
+# In[ ]:
+
+
+
+
+
+# ##### String Concatenation
+
+# In[ ]:
+
+
+
+
+
+# ##### Type Conversion
+
+# In[ ]:
+
+
+
+
+
+# ##### input()
+
+# In[ ]:
+
+
+
+
+
+# ##### format()
+
+# In[ ]:
+
+
+
+
+
+# ##### Old Way (python 2)
+
+# In[ ]:
+
+
+
+
+
+# # In-Class Exercise 1 <br>
+# <p>Create a format statement that asks for color, year, make, model and prints out the results</p>
+
+# In[ ]:
+
+
+
+
+
+# ### If Statements
+
+# In[ ]:
+
+
+# Available operators: Greater(>), Less(<),Equal(==)
+# Greater or Equal(>=), Less or Equal (<=)
+
+# Truth Tree:
+# T && F = F
+# T && T = T
+# T || F = T
+# F || T = T
+# F || F = F
+
+
+# ##### 'is' keyword
+
+# In[ ]:
+
+
+
+
+
+# ##### 'in' keyword
+
+# In[ ]:
+
+
+
+
+
+# ##### 'not in' keyword'
+
+# In[ ]:
+
+
+
+
+
+# # In-Class Exercise 2 <br>
+# <p>Ask user for input, check to see if the letter 'p' is in the input</p>
+
+# In[ ]:
+
+
+
+
+
+# ## Using 'and'/'or' with If Statements
+
+# In[ ]:
+
+
+
+
+
+# ### Elif Statements
+
+# In[ ]:
+
+
+
+
+
+# ### Else Statements
+
+# In[ ]:
+
+
+# see above
+
+
+# ### For Loops
+
+# In[ ]:
+
+
+
+
+
+# ##### Using 'in' keyword
+
+# In[ ]:
+
+
+# see above
+
+
+# ##### Continue Statement
+
+# In[ ]:
+
+
+# will continue to next iteration
+
+
+# In[ ]:
+
+
+
+
+
+# ##### Break Statement
+
+# In[ ]:
+
+
+# will break out of current loop
+
+
+# In[ ]:
+
+
+
+
+
+# ##### Pass Statement
+
+# In[ ]:
+
+
+# mostly used as a placeholder, and will continue on same iteration
+
+
+# In[ ]:
+
+
+
+
+
+# ##### Double For Loops
+
+# In[ ]:
+
+
+
+
+
+# ### While Loops
+
+# In[ ]:
+
+
+
+
+
+# ##### Looping 'While True'
+
+# In[ ]:
+
+
+
+
+
+# ##### While & For Loops Used Together
+
+# In[ ]:
+
+
+
+
+
+# ### Built-In Functions
+
+# ##### range()
+
+# In[ ]:
+
+
+
+
+
+# ##### len()
+
+# In[ ]:
+
+
+
+
+
+# ##### help()
+
+# In[ ]:
+
+
+
+
+
+# ##### isinstance()
+
+# In[ ]:
+
+
+
+
+
+# ##### abs()
+
+# In[ ]:
+
+
+
+
+
+# ### Try and Except
+
+# In[ ]:
+
+
+
+
+
+# ### Lists
+
+# ##### Declaring Lists
+
+# In[ ]:
+
+
+
+
+
+# ##### Indexing a List
+
+# In[ ]:
+
+
+
+
+
+# ##### .append()
+
+# In[ ]:
+
+
+
+
+
+# ##### .insert()
+
+# In[ ]:
+
+
+
+
+
+# ##### .pop()
+
+# In[ ]:
+
+
+
+
+
+# 
+# ##### .remove()
+
+# In[ ]:
+
+
+
+
+
+# ##### del()
+
+# In[ ]:
+
+
+
+
+
+# ##### Concatenating Two Lists
+
+# In[ ]:
+
+
+
+
+
+# ##### Lists Within Lists
+
+# In[ ]:
+
+
+
+
+
+# ##### Looping Through Lists
+
+# In[ ]:
+
+
+
+
+
+# ## Exercise #1 <br>
+# <p>Cube Number Test... Print out all cubed numbers up to the total value 1000. Meaning that if the cubed number is over 1000 break the loop.</p>
+
+# In[3]:
+
+
+base= 1
+if true
+    cube = base**3
+    print (cube)
+    base += 1
+    if cube > 1000:
+        break
+
+
+# ## Exercise #2 <br>
+# <p>Get first prime numbers up to 100</p>
+
+# In[4]:
+
+
+for num in range(1, 101):
+    count = 0
+    
+    for i in range(2, (num//2 + 1)):
+        if num % i == 1:
+            count = count + 1
+            break
+    if (count == 0 and num  != 1):
+            print(" %d" %num, end = ' ')
+
+
+# # Exercise 3 <br>
+# <p>Take in a users input for their age, if they are younger than 18 print kids, if they're 18 to 65 print adults, else print seniors</p>
+
+# In[ ]:
+
+
+age=(input('What is your age? '))
+
+age = int(age)
+
+if age < 18:
+    print('kids')
+    
+elif age < 65:
+    print('adults')
+else:
+    print('seniors')
+
+
+# In[ ]:
+
+
+
+
